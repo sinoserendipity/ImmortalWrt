315b840ca93b8a7884af4f94c3163b4547c9b2f3
sinoserendipity <sinoserendipity@gmail.com>
2025-08-07 18:04:17 -0400

    Update build.sh

--- build.sh
+++ build.sh
@@ -26,11 +26,12 @@ else
   # ============= 同步第三方插件库==============
   # 同步第三方软件仓库run/ipk
   echo "🔄 正在同步第三方软件仓库 Cloning run file repo..."
-  git clone --depth=1 --branch mystore https://github.com/sinoserendipity/store.git /tmp/store-run-repo
+  # git clone --depth=1 --branch mystore https://github.com/sinoserendipity/store.git /tmp/store-run-repo
 
   # 拷贝 run/x86 下所有 run 文件和ipk文件 到 extra-packages 目录
   mkdir -p /home/build/immortalwrt/extra-packages
-  cp -r /tmp/store-run-repo/run/x86/* /home/build/immortalwrt/extra-packages/
+  cp -r /home/build/immortalwrt/apps/* /home/build/immortalwrt/extra-packages/
+  # cp -r /tmp/store-run-repo/run/x86/* /home/build/immortalwrt/extra-packages/
 
   echo "✅ Run files copied to extra-packages:"
   ls -lh /home/build/immortalwrt/extra-packages/*.run
